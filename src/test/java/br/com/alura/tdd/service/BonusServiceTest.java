cff1ab96dd34b321f783f1447deea2a1006c7821
amandapazetti <amandapazettiperes@gmail.com>
2023-03-27 11:11:09 -0300

    'testes'

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -17,14 +17,32 @@ public class BonusServiceTest {
              /* criando um varialvel e atribuindo valores atraves de um metodo já criado que irá
              receber como argumento um objeto classe e dentro desse objeto será atribuido valores para os atributos.
               */
-            BigDecimal bonus =  service.calcularBonus (new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+             BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
 
             /* O método abaixo compara dois valores e verifica se eles são iguais,
              lançando uma exceção se eles não forem iguais.
              */
 
-                 assertEquals(BigDecimal.ZERO, bonus);
+             assertEquals(new BigDecimal("0.00"), bonus);
          }
+
+    @Test
+    void bonusDeveriaSer10PorCentoParaSalario() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus (new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500.00")));
+
+        assertEquals(new BigDecimal ("250.00"), bonus);
+    }
+
+    @Test
+    /*Esse teste está verificando se o bônus calculado pelo método "calcularBonus" da classe "BonusService" para um funcionário com
+     salário de exatamente 1000 reais é de 10% desse valor*/
+    void bonusDeveriaSer10PorCentoParaSalarioDeExatamente1000() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus (new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("1000.00")));
+
+        assertEquals(new BigDecimal ("100.00"), bonus);
+    }
 
      }
 
